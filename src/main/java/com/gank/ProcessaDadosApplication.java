caea4cdcb53d070a4130331de3186b72407d326b
Daniel dos Santos Rocha <daniel.rocha@riocard.com>
2017-01-19 11:19:24 -0200

    commit inicial

---
+++ src/main/java/com/gank/ProcessaDadosApplication.java
@@ -0,0 +1,12 @@
+package com.gank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessaDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessaDadosApplication.class, args);
+	}
+}
